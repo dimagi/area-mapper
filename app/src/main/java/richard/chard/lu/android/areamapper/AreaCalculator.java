da1a31515b3e806ed4e844a3783f62975b39f796
Richard Lu <lu16j@mit.edu>
2015-01-08 13:48:14 -0500

    Fill polygon

--- app/src/main/java/richard/chard/lu/android/areamapper/AreaCalculator.java
+++ app/src/main/java/richard/chard/lu/android/areamapper/AreaCalculator.java
@@ -53,7 +53,10 @@ public class AreaCalculator {
     }
 
     public PolygonOptions getPolygonOptions() {
-        return new PolygonOptions().addAll(boundingLatLngs);
+        return new PolygonOptions()
+                .addAll(boundingLatLngs)
+                .strokeColor(R.color.lightblue_900)
+                .fillColor(R.color.cyan_100_transparent);
     }
 
 }
