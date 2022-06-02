e7b510836753dfaac675fb3161e97512c40e2dea
Ahmad Vazirna <avazirna@gmail.com>
2022-06-02 09:26:57 +0200

    Add custom application class

---
+++ app/src/main/java/richard/chard/lu/android/areamapper/AreaMapperApplication.java
@@ -0,0 +1,11 @@
+package richard.chard.lu.android.areamapper;
+
+import android.app.Application;
+
+public class AreaMapperApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
