ee22e7aa801d55d79d381e7f134593c2ddee5827
Ahmad Vazirna <avazirna@gmail.com>
2022-06-03 15:25:02 +0200

    Change OnImageSavedListener to return a File object instead of a path to the file
    This change is necessary to allow the File Provider to generate a URI for the File object based on the application context

--- app/src/main/java/richard/chard/lu/android/areamapper/SaveImageAsyncTask.java
+++ app/src/main/java/richard/chard/lu/android/areamapper/SaveImageAsyncTask.java
@@ -18,7 +18,7 @@ public class SaveImageAsyncTask extends AsyncTask<Void, Void, Void> {
 
     public interface OnImageSavedListener {
 
-        public void onImageSaved(String filePath);
+        public void onImageSaved(File imageFile);
 
     }
 
@@ -31,7 +31,7 @@ public class SaveImageAsyncTask extends AsyncTask<Void, Void, Void> {
     private final String filePrefix;
     private final String fileSuffix;
     private final OnImageSavedListener listener;
-    private String filePath;
+    private File imageFile;
 
     public SaveImageAsyncTask(
             Bitmap bitmap,
@@ -89,7 +89,7 @@ public class SaveImageAsyncTask extends AsyncTask<Void, Void, Void> {
 
         // Write file
 
-        File imageFile = new File(imageFolder, imageFileName);
+        imageFile = new File(imageFolder, imageFileName);
 
         try {
             FileOutputStream fileOutputStream = new FileOutputStream(imageFile);
@@ -99,8 +99,6 @@ public class SaveImageAsyncTask extends AsyncTask<Void, Void, Void> {
             throw new RuntimeException(ioe);
         }
 
-        filePath = imageFile.getPath();
-
         LOG.trace("Exit");
         return null;
     }
@@ -109,7 +107,7 @@ public class SaveImageAsyncTask extends AsyncTask<Void, Void, Void> {
     protected void onPostExecute(Void aVoid) {
         LOG.trace("Entry");
 
-        listener.onImageSaved(filePath);
+        listener.onImageSaved(imageFile);
 
         LOG.trace("Exit");
     }
